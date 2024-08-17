207bb4f65d298702664263b13829ba1001c01196
Ricardo Chino <ricardoychino@gmail.com>
2024-08-17 17:54:25 -0300

    Template for replies

---
+++ src/components/CommentItemReplies.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="replies">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.replies {
+  border-left: 2px solid $neutral-400;
+  padding-left: 5%;
+
+  @media screen and (min-width: $bp-medium) {
+    margin-left: 35px;
+    padding-left: 35px;
+  }
+}
+</style>
