90853ab4adf3b453a79a30368763fcca9d1dd7e2
Ricardo Chino <ricardoychino@gmail.com>
2024-08-17 11:42:11 -0300

    Button hover changed to match the provided design choice

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import '@/assets/css/variables.scss';`
+        additionalData: `@use 'sass:map';@import '@/assets/css/variables.scss';`
       }
     }
   },
