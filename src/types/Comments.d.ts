d06242b9a8187cb2ad6353dc598330f1c29b15d0
Ricardo Chino <ricardoychino@gmail.com>
2024-08-17 19:02:20 -0300

    Comment type

--- src/types/Comments.d.ts
+++ src/types/Comments.d.ts
@@ -1,5 +1,15 @@
 type ImageFormats = 'png' | 'webp'
+
 export type User = {
   username: string,
   image: Record<ImageFormats, string>
+}
+
+export type Comment = {
+  id: number,
+  content: string,
+  createdAt: string // Date
+  score: number,
+  user: User,
+  replies: Comment[]
 }
