c498aa32f621b8f6ab02db3faddf8c1bc69c5ebe
Ricardo Chino <ricardoychino@gmail.com>
2024-08-17 17:08:15 -0300

    Rating component

---
+++ src/components/CommentItemRatings.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    rating: number
+    disabled?: boolean
+  }>(),
+  {
+    rating: 0,
+    boolean: false
+  }
+)
+
+const emit = defineEmits<{
+  increase: []
+  decrease: []
+}>()
+
+const handlePlusClick = () => {
+  if (!props.disabled) {
+    emit('increase')
+  }
+}
+
+const handleMinusClick = () => {
+  if (!props.disabled) {
+    emit('decrease')
+  }
+}
+</script>
+
+<template>
+  <div class="rating-wrapper">
+    <button class="primary-inverse flat-btn" :disabled="disabled" @click="handlePlusClick">
+      &plus;
+    </button>
+    <span class="counter">{{ rating }}</span>
+    <button class="primary-inverse flat-btn" :disabled="disabled" @click="handleMinusClick">
+      &minus;
+    </button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@mixin counter-block {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 5px;
+}
+.rating-wrapper {
+  display: inline-flex;
+  align-items: center;
+  color: $color-primary;
+  background-color: $neutral-400;
+  border-radius: 10px;
+  font-weight: 500;
+  overflow: hidden;
+
+  .counter {
+    @include counter-block;
+
+    font-weight: 500;
+    min-height: 30px;
+    @media screen and (min-width: $bp-medium) {
+      min-width: 42px;
+    }
+  }
+
+  button {
+    @include counter-block;
+
+    font-size: 20px;
+    font-weight: 700;
+    min-width: 30px;
+    min-height: 30px;
+  }
+
+  @media screen and (min-width: $bp-medium) {
+    flex-direction: column;
+  }
+}
+</style>
