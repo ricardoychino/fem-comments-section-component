4e01f5b4c622d73f82751a7656eb1eb498f95ed6
Ricardo Chino <ricardoychino@gmail.com>
2024-08-17 14:49:37 -0300

    TextArea with dynamic height component

---
+++ src/components/AutoHeightTextArea.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { ref, watch, nextTick, onMounted } from 'vue'
+
+withDefaults(
+  defineProps<{
+    rows?: string | number
+  }>(),
+  {
+    rows: 3
+  }
+)
+
+const modelValue = defineModel<string>()
+
+const el = ref<HTMLTextAreaElement | null>(null)
+
+const adjustSize = async () => {
+  if (!el.value) return
+
+  el.value.style.height = 'auto'
+  await nextTick()
+  el.value.style.height = `${el.value.scrollHeight}px`
+}
+
+watch(modelValue, adjustSize)
+
+onMounted(adjustSize)
+</script>
+
+<template>
+  <textarea ref="el" v-model="modelValue" v-bind="$attrs" :rows="rows"></textarea>
+</template>
+
+<style lang="scss" scoped>
+textarea {
+  font-family: inherit;
+  font-size: initial;
+  width: 100%;
+  overflow: hidden;
+  border: 1px solid $neutral-900;
+  border-radius: 10px;
+  padding: 10px 20px;
+
+  &:focus,
+  &:focus-visible {
+    border-color: $color-primary;
+  }
+}
+</style>
