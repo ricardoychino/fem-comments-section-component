72488e93b4eea322d9f8fa1d8d443fd00c69c77b
Ricardo Chino <ricardoychino@gmail.com>
2024-08-18 13:12:18 -0300

    Close icon

--- src/components/ButtonIcon.vue
+++ src/components/ButtonIcon.vue
@@ -4,7 +4,7 @@ import { defineAsyncComponent } from 'vue'
 /* Solution by this article: https://mkay11.medium.com/heres-a-simple-solution-to-incorporate-inline-svgs-in-vue-vite-da5897a480f7 */
 
 const props = defineProps<{
-  type: 'delete' | 'edit' | 'minus' | 'plus' | 'reply'
+  type: 'delete' | 'edit' | 'minus' | 'plus' | 'reply' | 'close'
 }>()
 
 const Icon = defineAsyncComponent(() => import(`@/assets/icons/icon-${props.type}.svg`))
